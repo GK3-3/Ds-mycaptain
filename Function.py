a6e25dac05dce69c2dd0eaf6f06995622da2fd7b
GK3-3 <82406672+GK3-3@users.noreply.github.com>
2021-11-18 08:29:56 +0530

    Update Function.py

--- Function.py
+++ Function.py
@@ -1,9 +1,21 @@
-s=input()
+s=input("Please Enter a string")
 def most_frequent(string):
     l=[]
     for i in range(len(s)):
         l.append(s[i])
     se=set(l)
+    l1=[]
     for i in se:
-        print(i,"=",l.count(i))
+        l1.append(l.count(i))
+    se1=set(l1)
+    d={}
+    l2=[]
+    for j in se1:
+        for i in se:
+            if l.count(i)==j:
+                l2.append(i)
+                d[i]=l.count(i)
+    l2.reverse()
+    for i in l2:
+        print(i,"=","%.2d" % d[i])
 most_frequent(s)
